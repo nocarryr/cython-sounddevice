8954d0fc7980a609a093a17b7fdd09c699bf73cc
nocarryr <matt@nomadic-recording.com>
2019-06-13 11:24:50 -0500

    Use alsa dummy module instead of jackd-dummy

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,5 @@
 import os
-from pathlib import Path
 import time
-import subprocess
-import shlex
 import pytest
 
 from cysounddevice import types
@@ -53,110 +50,13 @@ def sample_format(request):
 def nchannels(request):
     return request.param
 
-@pytest.fixture(scope='session')
-def _worker_id(request):
-    if hasattr(request.config, "workerinput"):
-        return request.config.workerinput["workerid"]
-    elif hasattr(request.config, 'slaveinput'):
-        return request.config.slaveinput['slaveid']
-    else:
-        return "master"
 
 @pytest.fixture
-def port_audio(jackd_server, _worker_id):
+def port_audio():
     pa = PortAudio()
     with pa:
         yield pa
-    time.sleep(.1)
+        print('EXITING PORTAUDIO')
     assert not pa._initialized
-
-class JackDServer(object):
-    def __init__(self, server_name):
-        self.server_name = server_name
-        self.proc = None
-    def _jack_wait(self, timeout=None, for_quit=False):
-        if timeout is None:
-            opt_str = '--check'
-        else:
-            opt_str = f'--wait --timeout {timeout}'
-        cmdstr = f'jack_wait --server {self.server_name} {opt_str}'
-        try:
-            resp = subprocess.check_output(shlex.split(cmdstr))
-        except subprocess.CalledProcessError as e:
-            if timeout is not None:
-                raise
-            return False
-        if isinstance(resp, bytes):
-            resp = resp.decode('UTF-8')
-        return 'server is available' in resp
-    def is_running(self):
-        return self._jack_wait()
-    def wait_for_start(self):
-        num_waits = 0
-        while num_waits < 5:
-            try:
-                r = self._jack_wait(2)
-            except subprocess.CalledProcessError:
-                r = False
-            if r:
-                return True
-            num_waits += 1
-        return False
-    def wait_for_stop(self):
-        timeout = 2
-        cmdstr = f'jack_wait --server {self.server_name} --quit --timeout {timeout}'
-        num_waits = 0
-        while num_waits < 5:
-            try:
-                resp = subprocess.check_output(shlex.split(cmdstr))
-            except subprocess.CalledProcessError as e:
-                num_waits += 1
-                continue
-            if isinstance(resp, bytes):
-                resp = resp.decode('UTF-8')
-            if 'server is gone' in resp:
-                return True
-        return False
-    def start(self):
-        if self.is_running():
-            if self.proc is not None:
-                return
-            self.wait_for_stop()
-        cmdstr = f'jackd --no-realtime -n{self.server_name} -ddummy -r48000 -p1024'
-        self.proc = subprocess.Popen(shlex.split(cmdstr))
-        running = self.wait_for_start()
-        assert running is True
-    def stop(self):
-        p = self.proc
-        if p is None:
-            return
-        self.proc = None
-        p.terminate()
-        p.wait()
-        if self.is_running():
-            self.wait_for_stop()
-    def __enter__(self):
-        try:
-            self.start()
-        except:
-            self.stop()
-            raise
-        return self
-    def __exit__(self, *args):
-        self.stop()
-    def __repr__(self):
-        return f'<{self.__class__}: "{self}">'
-    def __str__(self):
-        return self.server_name
-
-@pytest.fixture
-def jackd_server(request, monkeypatch, _worker_id):
-    test_mod = request.node.name.split('[')[0]
-    test_name = request.node.name.split('[')[1].rstrip(']')
-    server_name = f'{test_mod}.{test_name}_{_worker_id}'
-
-    server = JackDServer(server_name)
-    with server:
-        monkeypatch.setenv('JACK_DEFAULT_SERVER', server_name)
-        monkeypatch.setenv('JACK_NO_START_SERVER', '1')
-        yield server.server_name
+    time.sleep(1)
+    print('COMPLETE')
